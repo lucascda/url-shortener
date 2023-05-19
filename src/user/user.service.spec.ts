891658b8282291902836d883375d4b43ae4c19de
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-05-18 21:20:11 -0300

    feat(user.service): ensure .create is called with correct params

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -15,4 +15,20 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('.create', () => {
+    it('should call itself with correct params', async () => {
+      const createUserInput = {
+        name: 'any_name',
+        email: 'any_mail@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      };
+      const createSpy = jest.spyOn(service, 'create');
+
+      await service.create(createUserInput);
+
+      expect(createSpy).toHaveBeenCalledWith(createUserInput);
+    });
+  });
 });
