c355cde4a024122c2f295011108471c10c6f2b32
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-05-26 20:38:52 -0300

    test(AuthService): move signInUserInput to stubs folder

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import { faker } from '@faker-js/faker';
 import { UnauthorizedError } from './errors/unauthorizedError';
 import { UserService } from 'src/user/user.service';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as bcrypt from 'bcrypt';
+import { signInUserInput } from 'src/test/stubs/user-stub';
 
 jest.mock('bcrypt', () => ({
   async compare(): Promise<boolean> {
@@ -38,10 +38,6 @@ describe('AuthService', () => {
 
   describe('when user is signing in', () => {
     it('should throw if user does not exists', async () => {
-      const signInUserInput = {
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-      };
       jest.spyOn(userServiceMock, 'find').mockImplementation(() => false);
 
       const promise = service.auth(signInUserInput);
@@ -50,11 +46,6 @@ describe('AuthService', () => {
     });
 
     it('should compare passwords', async () => {
-      const signInUserInput = {
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-      };
       jest.spyOn(userServiceMock, 'find').mockImplementation(() => ({
         ...signInUserInput,
         id: 1,
@@ -71,11 +62,6 @@ describe('AuthService', () => {
     });
 
     it('should throw if user password is wrong', async () => {
-      const signInUserInput = {
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-      };
       jest.spyOn(userServiceMock, 'find').mockImplementation(() => ({
         ...signInUserInput,
         id: 1,
--- src/test/stubs/user-stub.ts
+++ src/test/stubs/user-stub.ts
@@ -1,8 +1,14 @@
 import { faker } from '@faker-js/faker';
+
 const userPassword = faker.internet.password();
 export const createUserInput = {
   name: faker.person.fullName(),
   email: faker.internet.email(),
   password: userPassword,
   passwordConfirmation: userPassword,
+};
+
+export const signInUserInput = {
+  email: faker.internet.email(),
+  password: faker.internet.password(),
 };
