fb000b4f5034a516efc459ce8841a9c8ee7f9ccc
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-06-12 15:49:55 -0300

    test(PrismaUrlRepository-int): ensure it creates a new shorten url

---
+++ src/url/test/integration/prismaUrl.repository.int-spec.ts
@@ -0,0 +1,43 @@
+import { faker } from '@faker-js/faker';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUrlData } from 'src/url/dto/create-url.dto';
+import { PrismaUrlRepository } from 'src/url/prismaUrl.repository';
+
+describe('PrismaUrlRepository Integration Tests', () => {
+  let repository: PrismaUrlRepository;
+  let prisma: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaUrlRepository],
+      imports: [PrismaModule],
+    }).compile();
+
+    repository = module.get<PrismaUrlRepository>(PrismaUrlRepository);
+    prisma = module.get<PrismaService>(PrismaService);
+
+    await prisma.cleanDatabase();
+    await prisma.resetAutoIncrement();
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  it('should create new shorten url', async () => {
+    const createUrlInput: CreateUrlData = {
+      original_url: faker.internet.url(),
+      short_url: `${process.env.BASE_URL}/random_id`,
+    };
+
+    const createdUrl = await repository.create(createUrlInput);
+
+    expect(createdUrl).toEqual({
+      id: 1,
+      original_url: createUrlInput.original_url,
+      short_url: createUrlInput.short_url,
+    });
+  });
+});
