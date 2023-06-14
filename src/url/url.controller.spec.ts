b45671523fcf97f1a1fee4760b6622e36cff53c0
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-06-14 20:34:06 -0300

    feat(UrlController): ensure it creates a new url with correct data

--- src/url/url.controller.spec.ts
+++ src/url/url.controller.spec.ts
@@ -2,17 +2,23 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UrlController } from './url.controller';
 import { UrlService } from './url.service';
 import { PrismaUrlRepository } from './prismaUrl.repository';
+import { CreateUrlInputDto } from './dto/create-url.dto';
+import { faker } from '@faker-js/faker';
 
 describe('UrlController', () => {
   let controller: UrlController;
 
+  const baseURL = process.env.BASE_URL;
   const urlRepositoryMock = {};
+  const urlServiceMock = {
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UrlController],
       providers: [
-        UrlService,
+        { provide: UrlService, useValue: urlServiceMock },
         { provide: PrismaUrlRepository, useValue: urlRepositoryMock },
       ],
     }).compile();
@@ -22,5 +28,25 @@ describe('UrlController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a new url with correct data', async () => {
+    const createUrlInput: CreateUrlInputDto = {
+      original_url: faker.internet.url(),
+    };
+    const serviceSpy = jest.spyOn(urlServiceMock, 'create').mockResolvedValue({
+      id: 1,
+      original_url: createUrlInput.original_url,
+      short_url: `${baseURL}/random_id`,
+    });
+
+    const createdUrl = await controller.create(createUrlInput);
+
+    expect(serviceSpy).toBeCalledWith(createUrlInput);
+    expect(createdUrl).toEqual({
+      id: 1,
+      original_url: createUrlInput.original_url,
+      short_url: `${baseURL}/random_id`,
+    });
   });
 });
--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,7 +1,14 @@
 import { Controller } from '@nestjs/common';
 import { UrlService } from './url.service';
+import { CreateUrlInputDto, CreateUrlOutputDto } from './dto/create-url.dto';
 
 @Controller('url')
 export class UrlController {
   constructor(private readonly urlService: UrlService) {}
+
+  async create(
+    createUrlInputDto: CreateUrlInputDto,
+  ): Promise<CreateUrlOutputDto> {
+    return await this.urlService.create(createUrlInputDto);
+  }
 }
