2f21ebe0041d00a8f70936753a190b300f31b7d3
Lucas César <43433070+lucascda@users.noreply.github.com>
2024-01-23 18:44:04 -0300

    feat(AuthController): catch UnauthorizedError and throw UnauthorizedException

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,20 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInUserInputDto } from './dto/signInUser.dto';
+import { UnauthorizedError } from './errors/unauthorizedError';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  signIn(@Body() signInDto: SignInUserInputDto) {
-    return this.authService.signIn(signInDto);
+  @Post()
+  async signIn(@Body() signInDto: SignInUserInputDto) {
+    try {
+      return await this.authService.signIn(signInDto);
+    } catch (e) {
+      if (e instanceof UnauthorizedError) {
+        throw new UnauthorizedException();
+      }
+    }
   }
 }
